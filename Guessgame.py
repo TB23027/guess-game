99cae6ca7531d802fd244bcb518e56dcefa7def2
Torr Beaman <tb23027@my.westlake.school.nz>
2025-02-19 12:00:31 +1300

    prints numbers as it goes now

--- Guessgame.py
+++ Guessgame.py
@@ -1,6 +1,7 @@
 import random
 number_to_guess = random.randint(1,100)
 guesses = 0
+guesses_1 = 0
 guessed = []
 while True:
     try:
@@ -11,11 +12,14 @@ while True:
         elif guess_1 > number_to_guess and guess_1 < 101:
             print("Too high!")
             guesses += 1 
+            guessed.append(guess_1)
+            print(guessed)
 
         elif guess_1 < number_to_guess and guess_1 > 0:
             guesses += 1
             guessed.append(guess_1)
             print("Too low")
+            print(guessed)
 
         elif guess_1 == number_to_guess:
             guesses += 1
@@ -23,11 +27,14 @@ while True:
         
         else:
             print("Must be between 1 and 100 please!")
-            guesses += 1
+            guesses_1 += 1
         
 
     except ValueError:
         print("Enter a valid number!")
+        guesses_1 += 1
 
 print("Correct!")
-print(f"Number of guesses: {guesses}")
+print(guessed)
+print(f"Number of valid guesses: {guesses}")
+print(f"Number of invalid guesses: {guesses_1}")
